f6a6a5209016a2003b620180a5a2d84baf1f1785
kit-trowbridge-mk <kittrow.dev@gmail.com>
2026-04-16 16:04:26 +0100

    start new with just count_words

--- lib/personal_diary.py
+++ lib/personal_diary.py
@@ -1,7 +1,9 @@
 # A function called make_snippet that takes a string as an argument 
 # and returns the first five words and then a '...' if there are more than that.
+
+# Step 1
+# A function called count_words that takes a string as an argument 
+# and returns the number of words in that string.
+
 def count_words(string):
     return len(string.split())
-
-def make_snippet(string):
-    return count_words(string)
--- tests/test_personal_diary.py
+++ tests/test_personal_diary.py
@@ -1,23 +1,8 @@
-# A function called make_snippet that takes a string as an argument 
-# and returns the first five words and then a '...' if there are more than that.
-
 from lib.personal_diary import *
 
-# Given a string, it returns a string
-def test_make_snippet_returns_string():
-    result = make_snippet("Hello")
-    assert result == "Hello"
+# given a string of "hello there", count_words() can split the string into 
+# the words ["hello", "there"]
 
-# not sure what to test in between but it seems like big jump
-# need to test it can count how many words?
-
-def test_make_snippet_can_count_words():
-    result = make_snippet("Hello there")   
-    assert result == 2
-
-
-
-# Given a string of six words, it returns the first five
-# def test_make_snippet_returns_first_five_words_when_given_six():
-#     result = make_snippet("The quick brown fox jumps over")
-#     assert result == "The quick brown fox jumps"
+def test_count_words_can_identify_words():
+    result = count_words("Hello there")   
+    assert result == ["Hello", "there"]
